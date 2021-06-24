93bb98309709d5c8d2d74b54ea87440f648064a1
emmanguyen <emma.mphuong@gmail.com>
2021-06-23 17:27:21 -0700

    Disable Alert

--- components/layout.js
+++ components/layout.js
@@ -4,10 +4,10 @@ import Footer from '../components/footer'
 export default function Layout({ preview, children }) {
   return (
     <>
-      <div className="min-h-screen">
+      {/* <div className="min-h-screen">
         <Alert preview={preview} />
         <main>{children}</main>
-      </div>
+      </div> */}
       <Footer />
     </>
   )
