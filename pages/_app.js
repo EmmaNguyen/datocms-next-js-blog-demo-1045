5e271bf0b02f7e2c921c32c2d1bfbd54f4c3f6d3
emmanguyen <emma.mphuong@gmail.com>
2021-09-29 19:02:49 -0700

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/index.css'
+import Script from 'next/script'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script id="ga-load"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-B64QP05MRG`}
+        />
+
+      <Script id="ga-push" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-B64QP05MRG');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
